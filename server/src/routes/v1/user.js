6d3aeebdb3ba200646ad88dfd26a4be164ac11a1
Mosinmiloluwa Owoso <mosinmiloluwa.o@gmail.com>
2019-04-26 14:22:59 +0100

    create client signup

--- server/src/routes/v1/user.js
+++ server/src/routes/v1/user.js
@@ -4,6 +4,7 @@ import userController from '../../controllers/userController';
 const user = express.Router();
 
 user.post('/createuser', userController.createUser);
+user.post('/signup', userController.clientSignup);
 user.post('/login', userController.login);
 
 export default user;
